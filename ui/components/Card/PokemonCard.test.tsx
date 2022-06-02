94af6a3bc8e24d85eaf679af722efb0caf5411b1
Alan Escobedo <alanescobedo.dev@gmail.com>
2022-06-02 03:21:39 -0500

    Add test for PokemonCard

---
+++ ui/components/Card/PokemonCard.test.tsx
@@ -0,0 +1,35 @@
+import { render, screen } from '@testing-library/react'
+import { PokemonCard } from './PokemonCard'
+import { useRouter } from 'next/router'
+
+// TODO: Create a mock-file to put the mock data in
+jest.mock('next/router', () => ({
+  useRouter: jest.fn()
+}))
+const mockRouter = {
+  push: jest.fn()
+}
+const mockBasicPokemonData = {
+  name: 'bulbasaur',
+  url: 'https://pokeapi.co/api/v2/pokemon/1/',
+  id: '1',
+  img: 'https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/1.png',
+}
+
+describe('PokemonCard', () => {
+  it('should render', () => {
+    render(<PokemonCard pokemon={mockBasicPokemonData} />)
+    screen.getByText(mockBasicPokemonData.name)
+  })
+  it('shoudl redirect to the pokemon page if click in CardActionArea', () => {
+    (useRouter as jest.Mock).mockReturnValue(mockRouter)
+
+    render(<PokemonCard pokemon={mockBasicPokemonData} />)
+    const cardActionArea = screen.getAllByRole('button')[0]
+    cardActionArea.click()
+
+
+    expect(mockRouter.push).toHaveBeenCalledWith(`/name/${mockBasicPokemonData.name}`)
+
+  })
+})
