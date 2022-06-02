2c2a6bd393324e4f79813f61feedf64f80be0107
Alan Escobedo <alanescobedo.dev@gmail.com>
2022-06-01 19:31:35 -0500

    Add dynamic page [name] and update initial pages - _app add material-ui providers - document add config to use roboto-font - index function to read data from pokeapi - [name]  function to read data from pokeapi

---
+++ pages/name/[name].tsx
@@ -0,0 +1,55 @@
+import React from 'react'
+import { GetStaticPaths, GetStaticProps } from 'next'
+import { IPokemon } from '../../types/pokemon-info.types'
+import { getBasicAllPokemonsData, getFullPokemonData } from '../../utils/pokemon-request'
+
+interface PokemonByNamePageProps {
+  pokemon: IPokemon
+}
+
+const PokemonByNamePage = ({ pokemon }: PokemonByNamePageProps) => {
+  return (
+    <div>
+      {JSON.stringify(pokemon)}
+    </div>
+  )
+}
+
+export default PokemonByNamePage
+
+export const getStaticPaths: GetStaticPaths = async () => {
+  try {
+    const results = await getBasicAllPokemonsData()
+    const pokeNames = results?.map(({ name }) => name)
+
+    return {
+      paths: pokeNames.map(name => ({ params: { name } })),
+      fallback: 'blocking'
+    }
+  } catch (error) {
+    return {
+      paths: [],
+      fallback: 'blocking'
+    }
+  }
+}
+
+export const getStaticProps: GetStaticProps = async ({ params }) => {
+  const { name } = params as { name: string }
+  const pokemon = await getFullPokemonData({ query: name })
+
+  if (!pokemon) {
+    return {
+      redirect: {
+        destination: '/',
+        permanent: false
+      }
+    }
+  }
+
+  return {
+    props: {
+      pokemon
+    }
+  }
+}
